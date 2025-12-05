101ea0a0ec905eaeb042e87d0197c6c2ae9e6b12
bogdan <bogdan@hdi-vier-ce.de>
2025-12-05 15:51:59 +0100

    adding logs

--- src/handlers/pdf/get-formfields.js
+++ src/handlers/pdf/get-formfields.js
@@ -4,6 +4,7 @@ const {extractFormfields} = require('../../services/pdf/formfields');
 
 const getFormfields = async (req, res, next) => {
   try {
+      console.log(req.filePath);
     const jsonOutput = await extractFormfields(req.filePath);
     res.json(jsonOutput);
   }
